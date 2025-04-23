ecb5723cf0125e2502be06856d43d816384a69b6
Luisirk <luisirk.montoto@gmail.com>
2025-04-23 20:05:41 +0200

    Create a list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 
 function App() {
   const [facciones, setFacciones] = useState([]);
-  const [selectedFaction, setSelectedFaction] = useState(''); // Estado para la facción seleccionada
-  const [unidades, setUnidades] = useState([]); // Estado para las unidades de la facción seleccionada
+  const [selectedFaction, setSelectedFaction] = useState('');
+  const [unidades, setUnidades] = useState([]);
+  const [userList, setUserList] = useState([]); // Nuevo estado para la lista del usuario
   const [error, setError] = useState(null);
   const [loadingFacciones, setLoadingFacciones] = useState(true);
-  const [loadingUnidades, setLoadingUnidades] = useState(false); // Nuevo estado para la carga de unidades
+  const [loadingUnidades, setLoadingUnidades] = useState(false);
 
   useEffect(() => {
     setLoadingFacciones(true);
@@ -49,12 +50,20 @@ function App() {
           setLoadingUnidades(false);
         });
     } else {
-      setUnidades([]); 
+      setUnidades([]);
     }
-  }, [selectedFaction]); // Este efecto se ejecuta cuando cambia selectedFaction
+  }, [selectedFaction]);
 
   const handleFactionChange = (event) => {
     setSelectedFaction(event.target.value);
+  };
+
+  const handleAddToList = (unidad) => {
+    setUserList([...userList, unidad]); // Añade la unidad al array userList
+  };
+
+  const handleRemoveFromList = (unidadToRemove) => {
+    setUserList(userList.filter(unidad => unidad.id_unidad !== unidadToRemove.id_unidad));
   };
 
   if (loadingFacciones) {
@@ -88,11 +97,26 @@ function App() {
               {unidades.map(unidad => (
                 <li key={unidad.id_unidad}>
                   {unidad.nombre} (Puntos: {unidad.coste_puntos})
+                  <button onClick={() => handleAddToList(unidad)}>Añadir a la lista</button>
                 </li>
               ))}
             </ul>
           )}
         </div>
+      )}
+
+      <h2>Tu Lista:</h2>
+      {userList.length === 0 ? (
+        <p>Tu lista está vacía.</p>
+      ) : (
+        <ul>
+          {userList.map(unidadEnLista => (
+            <li key={unidadEnLista.id_unidad}>
+              {unidadEnLista.nombre} (Puntos: {unidadEnLista.coste_puntos})
+              <button onClick={() => handleRemoveFromList(unidadEnLista)}>Eliminar</button>
+            </li>
+          ))}
+        </ul>
       )}
     </div>
   );
